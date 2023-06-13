aba89c02d891a376a539558514fe49ad8a9b3f64
Ashish Pal <ashishpal1717@gmail.com>
2023-06-13 10:55:15 +0530

    Bcrypt errors resolved

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,23 @@ const jwt = require('jsonwebtoken');
 const {connection} = require('../config/db');
 const {sendSMS} = require('../utils/sendSMS');
 
+userRoute.post('/signup',async(req,res)=>{
+    const {unique_id,branch_id,role,name,email,contact,password,coordinates,mac_id,admin_otp,wrong_attempts,status}=req.body;
+    try {
+        const hashed=bcrypt.hashSync(password,4)
+        connection.query(`insert into member (unique_id,branch_id,role,name,email,contact,password,coordinates,mac_id,admin_otp,wrong_attempts,admin_doc,status) values ('${unique_id}','${branch_id}','${role}','${name}','${email}','${contact}','${hashed}','${coordinates}','${mac_id}','${admin_otp}','${wrong_attempts}', NOW(), '${status}')`,(err,result)=>{
+            if(err){
+                return res.send(err.message);
+            }
+            return res.send('signup_success')
+        })
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(400).json({err: 'Something went wrong!'});
+    }
+})
+
 userRoute.post('/login',async(req,res) => {
     const {contact,password,macID,coordinates}=req.body;
     try {
@@ -18,23 +35,27 @@ userRoute.post('/login',async(req,res) => {
             }
             else{
                 if(result.length){
-                    bcrypt.compare(password,result[0].password,(err,status)=>{
-                        if(err){
-                           return res.status(401).json({err: 'Invalid password! Unauthorized access'});
+                    bcrypt.compare(password,result[0].password,async(err,status)=>{
+                        // console.log(password,status,result[0].password)
+                        if(status){
+                            if(macID!==result[0].mac_id){
+                                // SEND OTP
+                                let otp = await sendSMS(result[0].contact);
+                                res.cookie('otp',otp,{expires: new Date(Date.now() + 600 * 1000)});
+                                res.cookie('contact',contact,{expires: new Date(Date.now() + 600 * 1000)});
+                                res.cookie('userID',result[0].id,{expires: new Date(Date.now() + 600 * 1000)});
+                                res.status(202).json({msg: 'OTP sent successfully. Valid for 10 minutes only'});
+                            }
+                            else{
+                                const token = jwt.sign({id:result[0].id},process.env.key);
+                                res.status(200).json({msg:'Logged in successfully',token});
+                            }
+                        }
+                        else{
+                            res.status(403).json({err: 'Invalid password'});
                         }
                     });
-                    if(macID!==result[0].mac_id){
-                        // SEND OTP
-                        let otp = await sendSMS(result[0].contact);
-                        res.cookie('otp',otp,{expires: new Date(Date.now() + 600 * 1000)});
-                        res.cookie('contact',contact,{expires: new Date(Date.now() + 600 * 1000)});
-                        res.cookie('userID',result[0].id,{expires: new Date(Date.now() + 600 * 1000)});
-                        res.status(202).json({msg: 'OTP sent successfully'});
-                    }
-                    else{
-                        const token = jwt.sign({id:result[0].id},process.env.key);
-                        res.status(200).json({msg:'Logged in successfully',token});
-                    }
+                    
                 }
                 else{ 
                     res.status(404).json({err: 'No user found!'})
